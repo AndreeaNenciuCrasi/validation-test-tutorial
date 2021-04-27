ab7941c608ee19042d43fa20d7f5db7b8da603d9
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-04-27 15:55:01 +0300

    first commit

---
+++ src/main/java/com/example/validationtutorial/User.java
@@ -0,0 +1,40 @@
+package com.example.validationtutorial;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public User(@NotBlank(message = "Name is mandatory") String name, @NotBlank(message = "Email is mandatory") String email) {
+        this.name = name;
+        this.email = email;
+
+
+        // standard constructors / setters / getters / toString
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/example/validationtutorial/ValidationTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
